ac426ff5616107c0b1fd61c027902cfa9d8e858c
Olivier <olivier.elophe@gmail.com>
2021-03-12 09:08:42 +0100

    init

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+from core.marvel import liste_perso
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    perso = liste_perso()
+    return render_template('index.html', titre='Mon Site', nom='Ilano El Clandestino', perso=perso)
+
+
+
+@app.route('/coucou/')
+def coucou():
+    return 'Coucou !'
+
+if __name__ == '__main__':
+    app.run()
